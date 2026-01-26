f8c831c75f3483f45022f5499532896685826253
Tom Bates <batesyuk@users.noreply.github.com>
2026-01-26 11:21:07 +0000

    GovKitExample (#14)
    * Added GovKitExample
    
    * Updated sonar properties
    
    * Removed missing targets

---
+++ GovKitExample/GovKitExample/GovKitExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GovKitExampleApp.swift
+//  GovKitExample
+//
+//  Created by Thomas Bates on 23/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct GovKitExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
