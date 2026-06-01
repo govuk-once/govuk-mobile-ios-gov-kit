17b37ed0fe7309b79b9beddadda380be1dd08c12
joshdubey <95635393+joshdubey@users.noreply.github.com>
2026-06-01 12:11:14 +0100

    GOVUKAPP-3221: Fix GovKitUI test coverage (#31)
    * Create a GovKitUITests test target for GovKitExample and copy tests from GovKitUI
    Remove AdaptiveStackTests as they are no longer valid
    
    * Update testplan

---
+++ GovKitExample/GovKitUITests/GovKitUITests.swift
@@ -0,0 +1,18 @@
+//
+//  GovKitUITests.swift
+//  GovKitUITests
+//
+//  Created by Dubey, Josh on 26/05/2026.
+//
+
+import Testing
+
+struct GovKitUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
